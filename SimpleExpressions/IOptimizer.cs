76f92ba96260f2c4845a9e4cc136408cd92c7995
Alexander <45169858+chuggun@users.noreply.github.com>
2018-12-18 23:09:25 +0600

    Update IOptimizer.cs

--- SimpleExpressions/IOptimizer.cs
+++ SimpleExpressions/IOptimizer.cs
@@ -7,7 +7,7 @@ namespace SimpleExpressions
         /// <summary>
         /// Tryes to build optimized expression
         /// </summary>
-        /// <typeparam name="T">Expression type to ease lambda compilation</typeparam>
+        /// <typeparam name="T">Expression type to ease further lambda compilation</typeparam>
         /// <param name="sourceExp"></param>
         /// <returns></returns>
         bool TryBuild<T>(Expression<T> sourceExp, out Expression<T> optimizedExp);
